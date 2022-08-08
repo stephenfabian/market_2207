6128735a02ed27a0b97982f89a7bc04ab4ed6ec3
Stephen Fabian <fabianstephen@gmail.com>
2022-08-08 09:25:26 -0600

    test: tests for it1

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -10,6 +10,24 @@ describe Vendor do
     expect(vendor).to be_an_instance_of(Vendor)
     expect(vendor.name).to eq("Rocky Mountain Fresh")
     expect(vendor.inventory).to eq({})
-    
+
+     item1 = Item.new({name: 'Peach', price: "$0.75"})
+     item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+
+    expect(vendor.check_stock(item1)).to eq(0)
+
+         vendor.stock(item1, 25)
+         vendor.stock(item1, 30)
+
+         expect(vendor.check_stock(item1)).to eq(55)
+
+         vendor.stock(item2, 12)
+
+         expected = { item1 => 55,
+                      item2 => 12
+
+                      }
+         expect(vendor.inventory).to eq(expected)
   end
 end
