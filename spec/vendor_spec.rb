0769e83b1ea33dff2a94f779317edb659a1c8bd5
Stephen Fabian <fabianstephen@gmail.com>
2022-08-08 09:14:09 -0600

    feat: item class initialize

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,11 @@ describe Vendor do
 
   it 'test' do
   
+    vendor = Vendor.new("Rocky Mountain Fresh")
   
+    expect(vendor).to be_an_instance_of(Vendor)
+    expect(vendor.name).to eq("Rocky Mountain Fresh")
+    expect(vendor.inventory).to eq({})
+    
   end
 end
